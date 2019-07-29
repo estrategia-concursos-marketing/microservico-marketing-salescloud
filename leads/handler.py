34108fd037ba2fe762350de306bf6e84806843d9
Israel Mendes <38436684+israelmendez232@users.noreply.github.com>
2019-07-29 11:23:23 -0300

    Novas modificações, inserções de CFG no código

--- leads/handler.py
+++ leads/handler.py
@@ -3,6 +3,7 @@ import json
 import FuelSDK
 import requests
 import ET_Client
+import configparser # remover pacote depois que subir o microserviço.
 
 """
  MICROSERVIÇO para levar as leads do formulário aos respectivos lugares. 
@@ -35,7 +36,21 @@ def marketingCloud():
     de = ET_Client.ET_DataExtension_Row()
     de.CustomerKey = DataExtensions[0][0]
     de.auth_stub = stubObj
-    de.props = Lead_Teste
+    de.props = {
+        "oid": Lead_Teste['oid'],
+        "retURL": Lead_Teste['retURL'],
+        "Cidade_OrigemIP__c": Lead_Teste['Cidade_OrigemIP__c'],
+        "Estado_OrigemIP__c": Lead_Teste['Estado_OrigemIP__c'],
+        "Modo_de_entrada__c": Lead_Teste['Modo_de_entrada__c'],
+        "lead_source": Lead_Teste['lead_source'],
+        "Area_de_Interesse__c": Lead_Teste['Area_de_Interesse__c'],
+        "Concurso_de_Interesse__c": Lead_Teste['Concurso_de_Interesse__c'],
+        "Interesse_Evento__c": Lead_Teste['Interesse_Evento__c'],
+        "recordType": Lead_Teste['recordType'],
+        "first_name": Lead_Teste['first_name'],
+        "email": Lead_Teste['email'],
+        "phone": Lead_Teste['phone']
+    }
     postResponse = de.post()
 
     print('Post Status: ' + str(postResponse.status))
@@ -44,34 +59,25 @@ def marketingCloud():
     print('Results: ' + str(postResponse.results))
 
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
-
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
-
 def salesCloud():
     url = "https://webto.salesforce.com/servlet/servlet.WebToLead"
-
     encoding = {"encoding":"UTF-8"}
 
-    payload = "oid=%2000D41000001Q9k8&retURL=https%3A%2F%2Fwww.estrategiaconcursos.com.br%2Fgratis%2Fsucesso-vade-mecum-delagado-pc-rj%2F&Cidade_OrigemIP__c=Barueri&Estado_OrigemIP__c=Sao%20Paulo&Modo_de_entrada__c=landing-page&lead_source=Landing%20Page&Area_de_Interesse__c=agencias-reguladoras&Concurso_de_Interesse__c=detran-sp&    Interesse_Evento__c=Teste-MIcroservico&recordType=01241000001AP21&first_name=Teste0951&email=israel.mendez232%40gmail.com&phone=(11)%2094469-1991"
+    payload = {
+        "oid": Lead_Teste['oid'],
+        "retURL": Lead_Teste['retURL'],
+        "Cidade_OrigemIP__c": Lead_Teste['Cidade_OrigemIP__c'],
+        "Estado_OrigemIP__c": Lead_Teste['Estado_OrigemIP__c'],
+        "Modo_de_entrada__c": Lead_Teste['Modo_de_entrada__c'],
+        "lead_source": Lead_Teste['lead_source'],
+        "Area_de_Interesse__c": Lead_Teste['Area_de_Interesse__c'],
+        "Concurso_de_Interesse__c": Lead_Teste['Concurso_de_Interesse__c'],
+        "Interesse_Evento__c": Lead_Teste['Interesse_Evento__c'],
+        "recordType": Lead_Teste['recordType'],
+        "first_name": Lead_Teste['first_name'],
+        "email": Lead_Teste['email'],
+        "phone": Lead_Teste['phone']
+    }
     headers = {
         'Content-Type': "application/x-www-form-urlencoded",
         'Accept': "*/*",
@@ -85,45 +91,88 @@ def salesCloud():
 
     response = requests.request("POST", url, data=payload, headers=headers, params=encoding)
 
+    # body = {
+    #     "message": "Contato inserido com sucesso!",
+    #     "input": event
+    # }
+    # response = {
+    #     "statusCode": 200,
+    #     "body": json.dumps(body)
+    # }
+    # return response
+
 
 def S3andCSV():
     x = 2
 
 def main():
-    salesCloud()
+    config = configparser.ConfigParser()
+    config.read(r"leads\cfg.cfg")
+    print(configparser.get('MC', 'clientid'))
     # emailValidator()
-# 
-    # marketingCloud()
-# 
-# 
+
+    # salesCloud()
+
+    # marketingCloud(basesLeads_Gerais, Leads_Teste)
+    # marketingCloud(basesTotal_Gerais, Leads_Teste)
+
     # S3andCSV()
+    
+    # with open('leads\config.json') as json_file:
+    #     data = json.load(json_file)
+    # print(data)
   
 
 
 # Variáveis externas no Lambda:
-DataExtensions = [ # Estrutura é [Chave Externa, Nome da Base]
-    ['03763DA8-E55A-4349-9E42-5354D8E3C176', 'TESTE-Microservico-Leads-Gerais-5'],  # Base simulada de Leads-Gerais-5
-    ['75B15A37-E175-401B-8982-7F149A464129', 'TESTE-Microservico-Total_Emails_Geral_']   # Base simulada de Total_Emails_Geral_
-]
+## estrutura é [Chave Externa, Nome da Base]
+basesLeads_Gerais = ['03763DA8-E55A-4349-9E42-5354D8E3C176', 'TESTE-Microservico-Leads-Gerais-5'] # Base simulada de Leads-Gerais-5
+basesTotal_Gerais = ['75B15A37-E175-401B-8982-7F149A464129', 'TESTE-Microservico-Total_Emails_Geral_'] # Base simulada de Total_Emails_Geral_
 
 Bases = {
-    'clientid': 'frd0kjo8x032pu3bcd4pxpn4',
-    'clientsecret': 'AD0fLFSqfrTkPnXpRCo5Nn2w',
-    'defaultwsdl': 'https://webservice.exacttarget.com/etframework.wsdl',
-    'authenticationurl': 'https://mck0g3r840gk4n89wnf1-q7jml7y.auth.marketingcloudapis.com/',
-    'baseapiurl': 'https://mck0g3r840gk4n89wnf1-q7jml7y.rest.marketingcloudapis.com/',
-    'soapendpoint': 'https://mck0g3r840gk4n89wnf1-q7jml7y.soap.marketingcloudapis.com/',
+    'clientid': '',
+    'clientsecret': '',
+    'defaultwsdl': '',
+    'authenticationurl': '',
+    'baseapiurl': '',
+    'soapendpoint': '',
      # 'wsdl_file_local_loc': r'<WSDL_PATH>/ExactTargetWSDL.xml',
     'useOAuth2Authentication': 'True',
-    'accountId': '100002066',
+    'accountId': '',
     # 'scope': '<PERMISSION_LIST>'
 }
 
 Lead_Teste = {
-    "Nome" : "Israel Mendes", 
-    "Email" : "teste123@gmail.com"
+    "oid": "00D41000001Q9k8",
+    "retURL": "https://www.estrategiaconcursos.com.br/gratis/sucesso-vade-mecum-delagado-pc-rj/",
+    "Cidade_OrigemIP__c": "Barueri",
+    "Estado_OrigemIP__c": "Sao Paulo",
+    "Modo_de_entrada__c": "landing-page",
+    "lead_source": "Landing Page",
+    "Area_de_Interesse__c": "agencias-reguladoras",
+    "Concurso_de_Interesse__c": "detran-sp",
+    "Interesse_Evento__c": "Teste-Microservico",
+    "recordType": "01241000001AP21",
+    "first_name": "1043",
+    "email": "israel.mendez232@gmail.com",
+    "phone": "(11) 94469-1991"
 }
 
 
 if __name__ == '__main__':
-  main()
+    main()
+
+
+# def hello(event, context):
+#     body = {
+#         "message": "Go Serverless v1.0! Your function executed successfully!",
+#         "input": event
+#     }
+# 
+#     response = {
+#         "statusCode": 200,
+#         "body": json.dumps(body)
+#     }
+# 
+#     return response
+# 
