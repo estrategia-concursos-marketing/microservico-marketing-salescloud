f857a0509b0938e7f5962984882c1f3001495631
Israel Mendes <38436684+israelmendez232@users.noreply.github.com>
2019-07-30 10:55:35 -0300

    Principal estrutura do serviço finalizada

--- leads/handler.py
+++ leads/handler.py
@@ -7,6 +7,10 @@ import requests
 import ET_Client
 import dns.resolver
 import configparser # remover pacote depois que subir o microserviço.
+from datetime import datetime
+
+config = configparser.ConfigParser()
+config.read("cfg.cfg")
 
 """
  MICROSERVIÇO para levar as leads do formulário aos respectivos lugares. 
@@ -26,102 +30,54 @@ import configparser # remover pacote depois que subir o microserviço.
 """
 
 
-def emailValidator(email):
-    # Etapas da Validação:
-    ## 1. Regex;
-    ## 2. DNS e SMTP.
+def emailValidator(Lead_Teste):
+    email = Lead_Teste['email']
+    splitAddress = email.split('@')
+    domain = str(splitAddress[1])
 
-    # REGEX:
-    regex = "^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,})$"
+    records = dns.resolver.query(domain, 'MX')
+    mxRecord = records[0].exchange
+    mxRecord = str(mxRecord)
 
-    def badSyntax(email):
-        match = re.match(regex, email)
-        if match == None:
-            return "Email falso. Revalidar."
-        else:
-            return "Email válidow!"
-            # return True
+    server = smtplib.SMTP()
+    server.set_debuglevel(0)
+    server.connect(mxRecord)
+    server.helo(server.local_hostname)
+    server.mail(email)
 
-    # DNS e SMTP:
-    def smtpDnsValidtion(email):
-        splitAddress = email.split('@')
-        domain = str(splitAddress[1])
+    code, message = server.rcpt(str(email))
+    server.quit()
 
-        records = dns.resolver.query(domain, 'MX')
-        mxRecord = records[0].exchange
-        mxRecord = str(mxRecord)
-
-        server = smtplib.SMTP()
-        server.set_debuglevel(0)
-
-        server.connect(mxRecord)
-        server.helo(server.local_hostname)
-        server.mail(email)
-        code, message = server.rcpt(str(email))
-        server.quit()
-
-        if code == 250:
-            return "existingEmail"
-        else:
-            return "notExistingEmail"
-
-    print(badSyntax(email))
-    print(smtpDnsValidtion(email))
-    
-
-def marketingCloud():
+    if code == 250:
+        return True
+        
+def marketingCloud(Bases, Lead_Teste):
     """
 
 
     """
 
     debug = False
-    stubObj = ET_Client.ET_Client(False, False, Bases)
+    stubObj = ET_Client.ET_Client(False, debug, Authentication)
     de = ET_Client.ET_DataExtension_Row()
-    de.CustomerKey = DataExtensions[0][0]
+    de.CustomerKey = Bases
     de.auth_stub = stubObj
-    de.props = {
-        "oid": config['MC']['oid'],
-        "retURL": config['MC']['retURL'],
-        "Cidade_OrigemIP__c": config['MC']['Cidade_OrigemIP__c'],
-        "Estado_OrigemIP__c": config['MC']['Estado_OrigemIP__c'],
-        "Modo_de_entrada__c": config['MC']['Modo_de_entrada__c'],
-        "lead_source": config['MC']['lead_source'],
-        "Area_de_Interesse__c": config['MC']['Area_de_Interesse__c'],
-        "Concurso_de_Interesse__c": config['MC']['Concurso_de_Interesse__c'],
-        "Interesse_Evento__c": config['MC']['Interesse_Evento__c'],
-        "recordType": config['MC']['recordType'],
-        "first_name": config['MC']['first_name'],
-        "email": config['MC']['email'],
-        "phone": config['MC']['phone']
-    }
+    de.props = props1 if de.CustomerKey == basesLeads_Gerais else props2
     postResponse = de.post()
 
-    print('Post Status: ' + str(postResponse.status))
-    print('Code: ' + str(postResponse.code))
-    print('Message: ' + str(postResponse.message))
+    # print('Post Status: ' + str(postResponse.status))
+    # print('Code: ' + str(postResponse.code))
+    # print('Message: ' + str(postResponse.message))
     print('Results: ' + str(postResponse.results))
 
+    print("Lead no MC com Sucesso")
+    return "Lead inserida no MC com Sucesso"
 
-def salesCloud():
+
+def salesCloud(Lead_Teste):
     url = "https://webto.salesforce.com/servlet/servlet.WebToLead"
     encoding = {"encoding":"UTF-8"}
 
-    payload = {
-        "oid": config['MC']['oid'],
-        "retURL": config['MC']['retURL'],
-        "Cidade_OrigemIP__c": config['MC']['Cidade_OrigemIP__c'],
-        "Estado_OrigemIP__c": config['MC']['Estado_OrigemIP__c'],
-        "Modo_de_entrada__c": config['MC']['Modo_de_entrada__c'],
-        "lead_source": config['MC']['lead_source'],
-        "Area_de_Interesse__c": config['MC']['Area_de_Interesse__c'],
-        "Concurso_de_Interesse__c": config['MC']['Concurso_de_Interesse__c'],
-        "Interesse_Evento__c": config['MC']['Interesse_Evento__c'],
-        "recordType": config['MC']['recordType'],
-        "first_name": config['MC']['first_name'],
-        "email": config['MC']['email'],
-        "phone": config['MC']['phone']
-    }
     headers = {
         'Content-Type': "application/x-www-form-urlencoded",
         'Accept': "*/*",
@@ -135,83 +91,119 @@ def salesCloud():
 
     response = requests.request("POST", url, data=payload, headers=headers, params=encoding)
 
-    # body = {
-    #     "message": "Contato inserido com sucesso!",
-    #     "input": event
-    # }
-    # response = {
-    #     "statusCode": 200,
-    #     "body": json.dumps(body)
-    # }
-    # return response
+    print("Lead no Salescloud com Sucesso")
+    return "Lead no Salescloud com Sucesso"
 
-
-def S3andCSV():
-    x = 2
 
 def main():
-    email = 'email@gmail.com'
-    emailValidator(email)
+    if emailValidator(Lead_Teste):
+            body = {
+                "message": "Email não existente. Retornar para o usuário.",
+                "input": Lead_Teste['email']
+            }
 
-    # salesCloud()
+            response = {
+                "statusCode": 200,
+                "body": json.dumps(body)
+            }
 
-    # marketingCloud(basesLeads_Gerais, Leads_Teste)
-    # marketingCloud(basesTotal_Gerais, Leads_Teste)
+            return response
+        else:
+            emailValidator(Lead_Teste)
 
-    # S3andCSV()
-  
-# Variáveis externas no Lambda:
+            salesCloud(Lead_Teste)
+
+            marketingCloud(basesLeads_Gerais, Lead_Teste)
+
+            marketingCloud(basesTotal_Gerais, Lead_Teste)
+
+    
+# ===> Variáveis externas no Lambda:
 ## estrutura é [Chave Externa, Nome da Base]
-basesLeads_Gerais = ['03763DA8-E55A-4349-9E42-5354D8E3C176', 'TESTE-Microservico-Leads-Gerais-5'] # Base simulada de Leads-Gerais-5
-basesTotal_Gerais = ['75B15A37-E175-401B-8982-7F149A464129', 'TESTE-Microservico-Total_Emails_Geral_'] # Base simulada de Total_Emails_Geral_
+basesLeads_Gerais = ['TESTE-Microservico-Leads-Gerais-5', 'TESTE-Microservico-Leads-Gerais-5'] # Base simulada de Leads-Gerais-5
+basesTotal_Gerais = ['TESTE-Microservico-Total_Emails_Geral_', 'TESTE-Microservico-Total_Emails_Geral_'] # Base simulada de Total_Emails_Geral_
 
-Bases = {
-    'clientid': '',
-    'clientsecret': '',
-    'defaultwsdl': '',
-    'authenticationurl': '',
-    'baseapiurl': '',
-    'soapendpoint': '',
+Lead_Teste = {
+    "oid": config['LEADS']['oid'],
+    "retURL": config['LEADS']['retURL'],
+    "Cidade_OrigemIP__c": config['LEADS']['Cidade_OrigemIP__c'],
+    "Estado_OrigemIP__c": config['LEADS']['Estado_OrigemIP__c'],
+    "Modo_de_entrada__c": config['LEADS']['Modo_de_entrada__c'],
+    "lead_source": config['LEADS']['lead_source'],
+    "Area_de_Interesse__c": config['LEADS']['Area_de_Interesse__c'],
+    "Concurso_de_Interesse__c": config['LEADS']['Concurso_de_Interesse__c'],
+    "Interesse_Evento__c": config['LEADS']['Interesse_Evento__c'],
+    "recordType": config['LEADS']['recordType'],
+    "first_name": config['LEADS']['first_name'],
+    "email": config['LEADS']['email'],
+    "phone": config['LEADS']['phone']
+}
+
+payload = {
+    "oid": Lead_Teste['oid'],
+    "retURL": Lead_Teste['retURL'],
+    "Cidade_OrigemIP__c": Lead_Teste['Cidade_OrigemIP__c'],
+    "Estado_OrigemIP__c": Lead_Teste['Estado_OrigemIP__c'],
+    "Modo_de_entrada__c": Lead_Teste['Modo_de_entrada__c'],
+    "lead_source": Lead_Teste['lead_source'],
+    "Area_de_Interesse__c": Lead_Teste['Area_de_Interesse__c'],
+    "Concurso_de_Interesse__c": Lead_Teste['Concurso_de_Interesse__c'],
+    "Interesse_Evento__c": Lead_Teste['Interesse_Evento__c'],
+    "recordType": Lead_Teste['recordType'],
+    "first_name": Lead_Teste['first_name'],
+    "email": Lead_Teste['email'],
+    "phone": Lead_Teste['phone']
+}
+
+Authentication = {
+    'clientid': config['MC']['clientid'],
+    'clientsecret': config['MC']['clientsecret'],
+    'defaultwsdl': config['MC']['defaultwsdl'],
+    'authenticationurl': config['MC']['authenticationurl'],
+    'baseapiurl': config['MC']['baseapiurl'],
+    'soapendpoint': config['MC']['soapendpoint'],
      # 'wsdl_file_local_loc': r'<WSDL_PATH>/ExactTargetWSDL.xml',
-    'useOAuth2Authentication': 'True',
-    'accountId': '',
+    'useOAuth2Authentication': config['MC']['useOAuth2Authentication'],
+    'accountId': config['MC']['accountId'],
     # 'scope': '<PERMISSION_LIST>'
 }
 
-Lead_Teste = {
-    "oid": "00D41000001Q9k8",
-    "retURL": "https://www.estrategiaconcursos.com.br/gratis/sucesso-vade-mecum-delagado-pc-rj/",
-    "Cidade_OrigemIP__c": "Barueri",
-    "Estado_OrigemIP__c": "Sao Paulo",
-    "Modo_de_entrada__c": "landing-page",
-    "lead_source": "Landing Page",
-    "Area_de_Interesse__c": "agencias-reguladoras",
-    "Concurso_de_Interesse__c": "detran-sp",
-    "Interesse_Evento__c": "Teste-Microservico",
-    "recordType": "01241000001AP21",
-    "first_name": "1043",
-    "email": "israel.mendez232@gmail.com",
-    "phone": "(11) 94469-1991"
+
+props1 = {
+    "Cidade de Origem do IP": Lead_Teste['Cidade_OrigemIP__c'],
+    "Estado de Origem do IP": Lead_Teste['Estado_OrigemIP__c'],
+    "Modo de entrada": Lead_Teste['Modo_de_entrada__c'],
+    "Origem do lead": Lead_Teste['lead_source'],
+    "Interesse - Área": Lead_Teste['Area_de_Interesse__c'],
+    "Interesse - Concurso": Lead_Teste['Concurso_de_Interesse__c'],
+    "Interesse - Evento": Lead_Teste['Interesse_Evento__c'],
+    "Nome": Lead_Teste['first_name'],
+    "Email": Lead_Teste['email'],
+    "Telefone": Lead_Teste['phone'],
+    "Data de criação": datetime.now().strftime("%d/%m/%Y"),
+    "Hora de Criação": datetime.now().strftime("%H:%M")
 }
 
-config = configparser.ConfigParser()
-config.read(r"C:\Users\israel.silva.ESTRATEGIA\Desktop\Arquivos\Código\microservico-marketing-salescloud\leads\cfg.cfg")
-
+props2 = {
+    "Email": Lead_Teste['email'],
+    "Nome": Lead_Teste['first_name']
+}
+# ===> Variáveis externas no Lambda:
 
 if __name__ == '__main__':
     main()
 
 
-# def hello(event, context):
-#     body = {
-#         "message": "Go Serverless v1.0! Your function executed successfully!",
-#         "input": event
-#     }
-# 
-#     response = {
-#         "statusCode": 200,
-#         "body": json.dumps(body)
-#     }
-# 
-#     return response
-# 
+def hello(event, context):
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
